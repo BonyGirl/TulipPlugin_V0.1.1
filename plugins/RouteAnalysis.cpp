9fdd4028057dbe338f6c02171a7f1e6fb688575d
BonyGirl <liuzhenzhenthea@hotmail.com>
2017-06-27 16:01:45 -0600

    Update RouteAnalysis.cpp

--- plugins/RouteAnalysis.cpp
+++ plugins/RouteAnalysis.cpp
@@ -101,14 +101,14 @@ bool RouteAnalysis::run(){
     const ib::fabric_t::entities_t &entities_map = fabric->get_entities();
     
     const ib::entity_t & source_node = entities_map.at(std::strtoull(getGuid->getNodeStringValue(nodes_guid[0]).c_str(), NULL, 0));
+    const ib::entity_t & target_node = entities_map.at(std::strtoull(getGuid->getNodeStringValue(nodes_guid[1]).c_str(), NULL, 0));
     
-     if (pluginProgress) {
+    if (pluginProgress) {
         pluginProgress->setComment("Found path source and target");
         pluginProgress->progress(3, STEPS);
-        cout<<"test124"<<endl;
+        cout<<target_node.guid<<endl;
+        cout<<source_node.guid<<endl;
     }
-    
-    const ib::entity_t & target_node = entities_map.at(std::strtoull(getGuid->getNodeStringValue(nodes_guid[1]).c_str(), NULL, 0));
     
     unsigned int myhops = fabric->count_hops(source_node,target_node);
     
