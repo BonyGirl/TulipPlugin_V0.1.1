bb65076fffb73930f6b455dd2cd163ba34417378
BonyGirl <liuzhenzhenthea@hotmail.com>
2017-07-01 12:07:11 -0600

    Update RouteAnalysis.cpp

--- plugins/RouteAnalysis.cpp
+++ plugins/RouteAnalysis.cpp
@@ -45,7 +45,7 @@ namespace ibp = infiniband::parser;
 bool RouteAnalysis::run(){
     assert(graph);
 
-    static const size_t STEPS=4;
+    static const size_t STEPS=3;
     if(pluginProgress){
         pluginProgress->showPreview(false);
         pluginProgress->setComment("Starting to Import Routes");
@@ -73,16 +73,30 @@ bool RouteAnalysis::run(){
     vector<tlp::node> nodes_guid;
 
     tlp::Iterator<tlp::node> *selections = selectBool->getNodesEqualTo(true,NULL);
-    const ib::fabric_t::entities_t &entities_mymap = fabric->get_entities();
+    const ib::fabric_t::entities_t &entities_map = fabric->get_entities();
     
     while(selections->hasNext()){
         const tlp::node &mynode = selections->next();
         nodes_guid.push_back(mynode);
     }
+        
+    //const unsigned long long int key1 = std::stol((getGuid->getNodeStringValue(nodes_guid[0])).c_str(),NULL,0);
+    //const unsigned long long int key2 = std::stol((getGuid->getNodeStringValue(nodes_guid[1])).c_str(),NULL,0);
     
-    const ib::entity_t & source_node = entities_mymap.at(std::strtoull(getGuid->getNodeStringValue(nodes_guid.front()).c_str(),NULL,0));
-   cout<<source_node.guid<<endl;
 
+    const ib::entity_t & source_node = entities_map.at(std::stol((getGuid->getNodeStringValue(nodes_guid[0])).c_str(),NULL,0));
+    const ib::entity_t & target_node = entities_map.at(std::stol((getGuid->getNodeStringValue(nodes_guid[1])).c_str(),NULL,0));
+
+    if (pluginProgress) {
+        pluginProgress->setComment("Found path source and target");
+        pluginProgress->progress(2, STEPS);
+        cout<<target_node.guid<<endl;
+        cout<<source_node.guid<<endl;
+    }
+
+    unsigned int myhops = fabric->count_hops(source_node,target_node);
+
+    cout<<"The Real Hops between the source and the target is: "<<myhops<<endl;
     if(pluginProgress)
     {
         pluginProgress->setComment("Print the Real hops");
