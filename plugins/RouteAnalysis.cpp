32bf8bd0d92d1dabd7503670cd0f07a6289b0d69
BonyGirl <liuzhenzhenthea@hotmail.com>
2017-07-03 15:26:13 -0600

    Update RouteAnalysis.cpp

--- plugins/RouteAnalysis.cpp
+++ plugins/RouteAnalysis.cpp
@@ -4,9 +4,9 @@
 
 #include<fstream>
 #include <algorithm>
-#include <set>
 #include <string>
-#include "RouteAnalysis.h"
+#include <set>
+#include "testtulip.h"
 
 #include <tulip/GlScene.h>
 #include <tulip/BooleanProperty.h>
@@ -22,23 +22,21 @@ using namespace tlp;
 
 PLUGIN(RouteAnalysis)
 
-static const char * paramHelp[]={
-
-HTML_HELP_OPEN() \
-HTML_HELP_DEF("type","String")\
-HTML_HELP_DEF("values","print the routing path")\
-HTML_HELP_DEF("default","print the routing path")\
-HTML_HELP_BODY() \
-HTML_HELP_CLOSE()
-
+static const char * paramHelp[] = {
+        // File to Open
+        HTML_HELP_OPEN() \
+  HTML_HELP_DEF( "type", "pathname" ) \
+  HTML_HELP_BODY() \
+  "Path to ibdiagnet2.fdbs file to import" \
+  HTML_HELP_CLOSE()
 };
 
-static const string IMPORT_TYPE_STRING=  "print the routing path";
-
-RouteAnalysis::RouteAnalysis(tlp::PluginContext *context)
-: tlp::Algorithm(context){
-    addInParameter<tlp::StringCollection>("Test",paramHelp[0],IMPORT_TYPE_STRING);
+RouteAnalysis::RouteAnalysis(tlp::PluginContext* context)
+        : tlp::Algorithm(context)
+{
+    addInParameter<std::string>("file::filename", paramHelp[0],"");
 }
+
 
 namespace ib = infiniband;
 namespace ibp = infiniband::parser;
@@ -46,15 +44,21 @@ namespace ibp = infiniband::parser;
 bool RouteAnalysis::run(){
     assert(graph);
 
-    static const size_t STEPS=3;
-    if(pluginProgress){
+    static const size_t STEPS = 5;
+    if(pluginProgress)
+    {
         pluginProgress->showPreview(false);
         pluginProgress->setComment("Starting to Import Routes");
-        pluginProgress->progress(0,STEPS);
+        pluginProgress->progress(0, STEPS);
     }
 
-    ib::tulip_fabric_t * const fabric = ib::tulip_fabric_t::find_fabric(graph, false);
+    /**
+     * while this does not import
+     * nodes/edges, it imports properties
+     * for an existing fabric
+     */
 
+    ib::tulip_fabric_t * const fabric = ib::tulip_fabric_t::find_fabric(graph, false);
     if(!fabric)
     {
         if(pluginProgress)
@@ -63,10 +67,50 @@ bool RouteAnalysis::run(){
         return false;
     }
 
-    if(pluginProgress){
-        pluginProgress->setComment("Found fabric");
-        pluginProgress->progress(1,STEPS);
+    if(pluginProgress)
+    {
+        pluginProgress->setComment("Found Fabric");
+        pluginProgress->progress(1, STEPS);
     }
+
+    /**
+     * Open file to read and import per type
+     */
+    std::string filename;
+
+    dataSet->get("file::filename", filename);
+    std::ifstream ifs(filename.c_str());
+    if(!ifs)
+    {
+        if(pluginProgress)
+            pluginProgress->setError("Unable open source file.");
+
+        return false;
+    }
+
+    if(pluginProgress)
+    {
+        pluginProgress->progress(2, STEPS);
+        pluginProgress->setComment("Parsing Routes.");
+    }
+
+    ibp::ibdiagnet_fwd_db parser;
+    if(!parser.parse(*fabric, ifs))
+    {
+        if(pluginProgress)
+            pluginProgress->setError("Unable parse routes file.");
+
+        return false;
+    }
+
+    if(pluginProgress)
+    {
+        pluginProgress->setComment("Parsing Routes complete.");
+        pluginProgress->progress(3, STEPS);
+    }
+
+    ifs.close();
+
 
     //find the source and target nodes of the path
     BooleanProperty *selectBool = graph->getLocalProperty<BooleanProperty>("viewSelection");
@@ -75,45 +119,55 @@ bool RouteAnalysis::run(){
 
     tlp::Iterator<tlp::node> *selections = selectBool->getNodesEqualTo(true,NULL);
     const ib::fabric_t::entities_t &entities_map = fabric->get_entities();
-    
+
     while(selections->hasNext()){
         const tlp::node &mynode = selections->next();
         nodes_guid.push_back(mynode);
     }
-        
+
     //const unsigned long long int key1 = std::stol((getGuid->getNodeStringValue(nodes_guid[0])).c_str(),NULL,0);
     //const unsigned long long int key2 = std::stol((getGuid->getNodeStringValue(nodes_guid[1])).c_str(),NULL,0);
 
     const ib::entity_t & source_node = entities_map.find(std::stol((getGuid->getNodeStringValue(nodes_guid[0])).c_str(),NULL,0))->second;
     const ib::entity_t & target_node = entities_map.find(std::stol((getGuid->getNodeStringValue(nodes_guid[1])).c_str(),NULL,0))->second;
-    
+
     ib::lid_t target_lid = target_node.lid();
-    cout<<"target_lid: "<<target_lid<<endl;
-    cout<<"target_guid: "<<target_node.guid<<endl;
-    
     std::vector<ib::entity_t> tmp;
     tmp.push_back(const_cast<ib::entity_t &> (source_node));
 
-    const ib::entity_t & temp = tmp.back();
-    cout<<"source_guid: "<<temp.guid<<endl;
-    
-    for (
-            ib::entity_t::routes_t::const_iterator
-                    ritr = temp.get_routes().begin(),
-                    reitr = temp.get_routes().end();
-            ritr != reitr;
-            ++ritr
-            ) {
-        
-        for(std::set<ib::lid_t>::iterator citr = ritr->second.begin();  citr != ritr->second.end(); citr++)
-            cout<< *citr <<endl;
+    while(tmp.back().lid()!= target_lid) {
+        const ib::entity_t & temp = tmp.back();
+        cout<<temp.guid<<endl;
+        for (
+                ib::entity_t::routes_t::const_iterator
+                        ritr = temp.get_routes().begin(),
+                        reitr = temp.get_routes().end();
+                ritr != reitr;
+                ++ritr
+                ) {
+            std::set<ib::lid_t>::const_iterator itr = ritr->second.find(target_lid);
+            if (itr != ritr->second.end()) {
+                const ib::entity_t::portmap_t::const_iterator port_itr = temp.ports.find(ritr->first);
+                if (port_itr != temp.ports.end()) {
+                    const ib::port_t *const port = port_itr->second;
+                    const ib::tulip_fabric_t::port_edges_t::const_iterator edge_itr = fabric->port_edges.find(
+                            const_cast<ib::port_t *>(port));
+                    if (edge_itr != fabric->port_edges.end()) {
+                        const tlp::edge &edge = edge_itr->second;
+                        const ib::entity_t &node = entities_map.find(std::stol((getGuid->getNodeStringValue(graph->target(edge))).c_str(), NULL, 0))->second;
+                        tmp.push_back(const_cast<ib::entity_t &> (node));
+                    }
+                }
+            }
+        }
     }
-    
+
+
     if (pluginProgress) {
         pluginProgress->setComment("Found path source and target");
-        pluginProgress->progress(2, STEPS);
+        pluginProgress->progress(4, STEPS);
     }
-    
+
     if(pluginProgress)
     {
         pluginProgress->setComment("Print the Real hops");
@@ -122,3 +176,4 @@ bool RouteAnalysis::run(){
 
     return true;
 }
+
