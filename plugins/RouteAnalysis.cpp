eddba8e9693cf94277889ef9bafa1c8eed28bd6b
BonyGirl <liuzhenzhenthea@hotmail.com>
2017-07-11 13:57:35 -0600

    Update RouteAnalysis.cpp

--- plugins/RouteAnalysis.cpp
+++ plugins/RouteAnalysis.cpp
@@ -308,8 +308,8 @@ bool RouteAnalysis::run(){
                             const tlp::edge &edge = edge_itr->second;
                             //setColor->setEdgeValue(edge, tlp::Color::SpringGreen);
                             selectBool->setEdgeValue(edge, true);
-                            const ib::entity_t &node = entities_map.find(std::stol((getGuid->getNodeStringValue(graph->target(edge))).c_str(), NULL, 0))->second;
-                            tmp.push_back(const_cast<ib::entity_t *> (&node));
+                            const ib::entity_t * node =  getMyEntity(graph->target(edge),fabric);
+                            tmp.push_back(const_cast<ib::entity_t *> (node));
                             count_hops++;
                         }
                     }
