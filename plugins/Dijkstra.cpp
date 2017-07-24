4efeefb5a16ae1078656ba6ec98e9793ea06345e
BonyGirl <liuzhenzhenthea@hotmail.com>
2017-07-24 10:17:45 -0600

    Update Dijkstra.cpp

--- plugins/Dijkstra.cpp
+++ plugins/Dijkstra.cpp
@@ -233,17 +233,22 @@ bool Dijkstra::run()
 
     tlp::Iterator<node> *selections = selectBool->getNodesEqualTo(true,NULL);
 
-    int path_node[2];
-    path_node[1]=0;// Default source node is 0
+    int path_node[1];
+    path_node[0]=0;// Default source node is 0
     int path_id = 0;
-    bool found_path = false;
 
     while(selections->hasNext()){
         const node &mynode = selections->next();
+         if(path_id>1)
+         {
+           if(pluginProgress)
+             pluginProgress->setError("More than one node are selected");
+
+           return false;
+         }
         path_node[path_id++] = mynode.id;
     }
 
-    if(path_id >1) found_path = true;
 
     nodes_map *graphAnalysis = new nodes_map(graph,v);
     //test first and then modify to select source by user
@@ -276,35 +281,6 @@ bool Dijkstra::run()
         const tlp::node &node = itnodes->next();
         const int &temp = mymap[node.id]->getDist();
         ibHop->setNodeValue(node, temp);
-    }
-
-    //show the found_path in the tulip
-    if(found_path)
-    {
-        mypath = graphAnalysis->tracePath(mymap,path_node[1],path_node[0]);
-        tlp::ColorProperty * resetColor = graph->getLocalProperty<tlp::ColorProperty>("viewColor");
-        itnodes = graph->getNodes();
-
-        while(itnodes->hasNext()){
-            const tlp::node &node = itnodes->next();
-            for(unsigned int ID : mypath){
-                if(node.id == ID){
-                    resetColor->setNodeValue(node, Color::SpringGreen);
-                    selectBool->setNodeValue(node, true);
-                }
-            }
-        }
-
-        for(unsigned int i = 0; i<mypath.size()-1; i++){
-            const tlp::node &source = find_node(mypath[i]);
-            tlp::Iterator<tlp::edge> *itedges = graph->getOutEdges(source);
-            while(itedges->hasNext()){
-                const tlp::edge &edge = itedges->next();
-                if(graph->target(edge).id == mypath[i+1])
-                    selectBool->setEdgeValue(edge, true);
-            }
-        }
-
     }
 
     if(pluginProgress)
